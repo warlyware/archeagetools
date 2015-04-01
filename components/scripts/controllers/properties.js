f06cd1c743452020b3ec9d6f15a0f8f3039459d4
Dan Ward <danward@gmail.com>
2015-04-01 17:03:37 -0400

    Added basic layout of properties page

--- components/scripts/controllers/properties.js
+++ components/scripts/controllers/properties.js
@@ -7,18 +7,65 @@ myApp.controller('PropCtrl', ['$scope', '$rootScope', '$state', '$stateParams', 
 	var userID = $rootScope.currentUser.$id;
 
 	var ref = new Firebase(FIREBASE_URL + 'users/' + userID + '/properties');
-	var propertiesObj = $firebaseObject(ref);
 	var propertiesArr = $firebaseArray(ref);
 
-	
 	$scope.properties = propertiesArr;
 	
-	propertiesObj.$loaded().then(function() {
-		console.log(propertiesObj);
+	propertiesArr.$loaded().then(function() {
+		statusCheck();
 	});
 
-			$timeout(function () {
-				console.log("bing!");
-			}, 5000);
+
+
+	var statusCheck = function() {
+		console.log("Status check starting...");
+		angular.forEach(propertiesArr, function (property, key) {
+			angular.forEach(property, function (val, key) {
+				if (val!==null){
+					if (val.propowner!==undefined) {
+						whichOwner = val.propowner;
+						whichProperty = key;
+						var ref = new Firebase(FIREBASE_URL + '/users/' + userID + '/properties/' + whichOwner + '/' + whichProperty + '/');
+						var whichPropObj = $firebaseObject(ref);
+						whichPropObj.$loaded().then(function() {
+							var dueunix;
+							var nowunix;
+							dueunix = whichPropObj.propduemoment;
+							nowunix = moment().unix().toString();
+							var oldduedate = whichPropObj.propduedateiso;
+							var demodate = moment(oldduedate).add(7, 'days');
+							var demodateiso = moment(demodate).toISOString();
+							var demodateformatted = moment(demodate).format('ddd, MMM Do [at] h:mm a');
+							var timeBetween = dueunix-nowunix;
+
+							//Set status to overdue
+						    if (nowunix > dueunix) {
+								whichPropObj.propstatus = 'overdue';
+								whichPropObj.propdemodateformatted = demodateformatted;
+								whichPropObj.propdemodate = demodateiso;
+							} else if (timeBetween < 86400 && timeBetween > 0) { // Set due soon												
+								whichPropObj.propstatus = 'duesoon';
+						    } else  if (timeBetween < 0) { //Or set paid
+								whichPropObj.propdemodate = '';
+								whichPropObj.propstatus = 'paid';
+						    }
+						});
+					}
+				}
+			});
+		});
+		console.log("Checked!");
+
+	}
+
+	$scope.selectProperty = function(key, property) {
+		$scope.owner = property.propowner
+		$scope.proptype = property.proptype
+		console.log(key + ', ' + property.propowner);
+	}	
+
+	// $timeout(function () {
+	// 	console.log("Status check!");
+	// }, 500);
 
 }]);
